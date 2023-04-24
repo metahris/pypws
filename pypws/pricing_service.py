67b52077adc94d15b891a8c38c7a8262e664a57d
metahris <simotahri1@gmail.com>
2023-04-25 00:53:18 +0200

    add pricing service client

---
+++ pypws/pricing_service.py
@@ -0,0 +1,30 @@
+import asyncio
+
+from base_api import BaseAPI
+from config import config
+from option import Priceable
+
+
+class OptionPricingService(BaseAPI):
+    def __init__(self, env='prod'):
+        super().__init__(config=config.pricing_service, env=env)
+        self._set_resource_url('pricing')
+
+    async def price(self, priceable: Priceable, batch_size: int, endpoint=''):
+        '''
+        :param priceable: Priceable object
+        :param batch_size: number of options per batch
+        :param endpoint: the pricing endpoint for the specified option
+        :return: list of priced options
+        '''
+        batches = [priceable.options[i:i + batch_size] for i in range(0, len(priceable.options), batch_size)]
+        async with self._client_session() as session:
+            tasks = []
+            for batch in batches:
+                task = asyncio.create_task(self._post_async(session=session, endpoint=endpoint, json={
+                    'options': batch,
+                    'data': priceable.market_data.to_dict()
+                }))
+                tasks.append(task)
+            results = await asyncio.gather(*tasks)
+            return results
